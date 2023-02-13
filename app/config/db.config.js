8a4c87fbfb70719f76566ea9eb87755f53d99544
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-02-13 15:27:35 +0200

    'main'

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,5 @@ module.exports = {
     HOST: "bld8xcgiul5z3betrtd5-mysql.services.clever-cloud.com",
     USER: "uryrpkrujlzwpptb",
     PASSWORD: "fHM1nWdH69sr7T4OFiN5",
-    DB: "testdb",
+    DB: "bld8xcgiul5z3betrtd5",
 }
